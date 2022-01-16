ba3a7d4125eb4cf71272ef4e34783f3c52f3cbfb
john <>
2022-01-17 03:28:09 +0800

    Front-end updated

---
+++ src/static/lib/mylib.js
@@ -0,0 +1,87 @@
+import { ethers } from "./ethers-5.1.esm.min.js";
+let signInBtn = document.querySelector('#sign-in-btn');
+let provider;
+let signer;
+let eth_chainids = ['1', '3', '4', '5','42'];
+
+try {
+  
+  provider = new ethers.providers.Web3Provider(window.ethereum);
+
+  signer = provider.getSigner();
+}
+catch {
+
+  signInBtn.parentNode.replaceChild(
+    document.createTextNode('Non Web3 browser detected. Please install MetaMask.'),
+    signInBtn
+    
+  );
+}
+
+var simpleAjax = function(data,type,url) { 
+
+  var promise = new Promise(function(resolve, reject){
+      var xhr = new XMLHttpRequest()
+      if (type === 'GET') {
+        url = url + '/?pb=' + data
+        xhr.open(type, url)
+        xhr.send();
+      }
+      else if (type === 'POST') {
+        xhr.open(type, url)
+        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhr.send(data);
+      }
+      xhr.onreadystatechange = function() {
+          if(xhr.readyState === 4) {
+            if((xhr.status >=200 && xhr.status < 300) || xhr.status === 304) {
+              resolve(JSON.parse(xhr.responseText))
+            } else {
+              reject(xhr)
+            }
+          }
+      }
+  })
+  return promise
+}
+
+let mylongin = async () => {
+
+    const current_chainId = window.ethereum.networkVersion
+    // console.log(current_chainId)
+    if (!eth_chainids.includes(current_chainId)){
+      alert("Please Choose the appropriate Ethereum network.") 
+      return
+    }
+
+    await provider.send("eth_requestAccounts", [])
+    let userAddress = await signer.getAddress()
+    let GetResp = await simpleAjax(userAddress, 'GET', '/api/userNonce')
+    let userTrackingAddress = GetResp.TrackingAddress
+    let userWalletTag = GetResp.WalletTag
+    let userEmailAddress = GetResp.EmailAddress
+    // console.log(GetResp)
+
+    let rawSignature = await signer.signMessage(GetResp.Nonce);
+    // console.log(rawSignature);
+    const obj1 = {};
+    obj1.pb = userAddress;
+    obj1.sig = rawSignature;
+    let data = JSON.stringify(obj1)
+    // console.log(data)
+    let PostResp = await simpleAjax(data, 'POST', '/api/login')
+    // console.log(PostResp)
+    if (PostResp.Authenticated === true)  
+    {
+      sessionStorage.setItem('token',JSON.stringify(PostResp.Token))
+      sessionStorage.setItem('userTrackingAddr',JSON.stringify(userTrackingAddress))
+      sessionStorage.setItem('userWalletTag',JSON.stringify(userWalletTag))
+      sessionStorage.setItem('userEmailAddr',JSON.stringify(userEmailAddress))
+      window.location.href='/api/content' 
+    }
+}
+
+signInBtn.addEventListener('click', mylongin);
+
+
---
+++ src/static/modules/common.js
@@ -0,0 +1,75 @@
+/**
+
+ @Name：layuiAdmin 公共业务
+ @Author：贤心
+ @Site：http://www.layui.com/admin/
+ @License：LPPL
+    
+ */
+ 
+layui.define(function(exports){
+  var $ = layui.$
+  ,layer = layui.layer
+  ,laytpl = layui.laytpl
+  ,setter = layui.setter
+  ,view = layui.view
+  ,admin = layui.admin
+  
+  //公共业务的逻辑处理可以写在此处，切换任何页面都会执行
+  //……
+  /**
+   * common.js
+   * @author 御风 <1945199284@qq.com>
+   */
+  layui.define(['layer'], function(exports) {
+    "use strict";
+
+    var $ = layui.jquery,
+        layer = layui.layer;
+
+    var common = {
+      /**
+       * 抛出一个异常错误信息
+       * @param {String} msg
+       */
+      throwError: function(msg) {
+        throw new Error(msg);
+        return;
+      },
+      /**
+       * 弹出一个错误提示
+       * @param {String} msg
+       */
+      msgError: function(msg) {
+        layer.msg(msg, {
+          icon: 5
+        });
+        return;
+      }
+    };
+
+    exports('common', common);
+  });
+  
+  
+  //退出
+  admin.events.logout = function(){
+    //执行退出接口
+    admin.req({
+      url: layui.setter.base + 'json/user/logout.js'
+      ,type: 'get'
+      ,data: {}
+      ,done: function(res){ //这里要说明一下：done 是只有 response 的 code 正常才会执行。而 succese 则是只要 http 为 200 就会执行
+        
+        //清空本地记录的 token，并跳转到登入页
+        admin.exit(function(){
+          location.href = 'user/login.html';
+        });
+      }
+    });
+  };
+
+  
+  //对外暴露的接口
+  exports('common', {});
+});
